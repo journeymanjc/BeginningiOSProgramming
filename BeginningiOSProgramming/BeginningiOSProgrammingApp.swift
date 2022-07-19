c302ed601adc688c106b402599033e83cd476c38
Jae Cho <mobilewiz@gmail.com>
2022-07-19 18:00:19 -0400

    Initial Commit

---
+++ BeginningiOSProgramming/BeginningiOSProgrammingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeginningiOSProgrammingApp.swift
+//  BeginningiOSProgramming
+//
+//  Created by Jae Cho on 7/19/22.
+//
+
+import SwiftUI
+
+@main
+struct BeginningiOSProgrammingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
