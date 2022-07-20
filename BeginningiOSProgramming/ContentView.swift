362b6f07042b2b858878927546c02f7b68eade18
Jae Cho <mobilewiz@gmail.com>
2022-07-19 21:05:20 -0400

    Guess these movies

--- BeginningiOSProgramming/ContentView.swift
+++ BeginningiOSProgramming/ContentView.swift
@@ -10,22 +10,46 @@ import AVFoundation
 
 struct ContentView: View {
     var body: some View {
-		 Button{
-			 let utterance = AVSpeechUtterance(string: "Hello world")
-			 utterance.voice = AVSpeechSynthesisVoice(language: "en-GB")
-			 let synthesizer = AVSpeechSynthesizer()
-			 synthesizer.speak(utterance)
-			 
-		 }label: {
-			 Text("Hello")
+		 VStack{
+			 Text("Guess These Movies")
 				 .fontWeight(.bold)
 				 .font(.system(.title,design: .rounded))
+			 Text("Can you guess the mvoie from these emojis? \n Tap the button to find out the answer")
+				 .fontWeight(.light)
+				 .font(.system(.caption))
+			 
+			 Button{
+				 speak(text: "Hello")
+			 }label: {
+				 Text("🤦🏻‍♂️👨🏻‍🦲🚗⌚️")
+					 .fontWeight(.bold)
+					 .font(.system(.title,design: .rounded))
+			 }
+			 .padding()
+			 .foregroundColor(.white)
+			 .background(Color.purple)
+			 .cornerRadius(20)
+			 
+			 Button{
+				 speak(text: "Spiderman")
+			 }label: {
+				 Text("🕷 🕺🏻")
+					 .fontWeight(.bold)
+					 .font(.system(.title,design: .rounded))
+			 }
+			 .padding()
+			 .foregroundColor(.white)
+			 .background(Color.purple)
+			 .cornerRadius(20)
 		 }
-		 .padding()
-		 .foregroundColor(.white)
-		 .background(Color.purple)
-		 .cornerRadius(20)
     }
+	func speak(text: String){
+		let utterance = AVSpeechUtterance(string: text)
+		utterance.voice = AVSpeechSynthesisVoice(language: "en-GB")
+		let synthesizer = AVSpeechSynthesizer()
+		synthesizer.speak(utterance)
+	}
+	
 }
 
 struct ContentView_Previews: PreviewProvider {
